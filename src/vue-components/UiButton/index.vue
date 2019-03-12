4639409eacead97ea25f47e6e139301ef5ebac38
Olga Pereverzeva <olja125@yandex.ru>
2019-03-12 12:44:30 +0300

    add segment, tab, slider, avatars

---
+++ src/vue-components/UiButton/index.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <button class="button button_large" type="submit">
+    <block name="buttonName">Test button Name</block>
+  </button>
+</template>
+
+
+<style>
+@import url("../../components/buttons/button-large.css");
+</style>
---
+++ src/vue-components/UiKitOverview/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Hello world!
+    <ui-button>
+      <block name="buttonName">Test</block>
+    </ui-button>
+  </div>
+</template>
+
+<script>
+import UiButton from "../UiButton";
+
+export default {
+  components: {
+    UiButton
+  }
+};
+</script>
